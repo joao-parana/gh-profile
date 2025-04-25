443d1958930fb0f05609480fca3983b57f2e51f1
joao-parana <joao.parana@gmail.com>
2025-04-24 22:56:56 -0300

    fix: Using Svelte version FIVE

--- src/interfaces/IRepositorio.ts
+++ src/interfaces/IRepositorio.ts
@@ -1,3 +1,4 @@
+// export default interface IRepositorio {
 export default interface IRepositorio {
   nome: string;
   url: string;
--- src/utils/montaUsuario.ts
+++ src/utils/montaUsuario.ts
@@ -1,14 +1,20 @@
-import type IRepositorio from '../interfaces/IRepositorio';
-import type IUsuario from '../interfaces/IUsuario';
+import type GhRepoResponse from "../interfaces/GhRepoResponse";
+import type IRepositorio from "../interfaces/IRepositorio";
+import type IUsuario from "../interfaces/IUsuario";
 
-export default function montaUsuario(dadosUsuario: any, dadosRepositorios: any): IUsuario {
-  const repositorios_recentes = dadosRepositorios.map((repositorio) => {
-    return {
-      nome: repositorio.name,
-      url: repositorio.html_url
-    } as IRepositorio;
-  });
-  
+export default function montaUsuario(
+  dadosUsuario: any,
+  dadosRepositorios: any,
+): IUsuario {
+  const repositorios_recentes = dadosRepositorios.map(
+    (repositorio: GhRepoResponse) => {
+      return {
+        nome: repositorio.name,
+        url: repositorio.html_url,
+      } as IRepositorio;
+    },
+  );
+
   return {
     avatar_url: dadosUsuario.avatar_url,
     login: dadosUsuario.login,
@@ -16,6 +22,6 @@ export default function montaUsuario(dadosUsuario: any, dadosRepositorios: any):
     perfil_url: dadosUsuario.html_url,
     repositorios_publicos: dadosUsuario.public_repos,
     seguidores: dadosUsuario.followers,
-    repositorios_recentes
+    repositorios_recentes,
   };
 }
